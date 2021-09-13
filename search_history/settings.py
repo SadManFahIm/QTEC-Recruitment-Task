99e5266fbe9f0dada1160c3c04088ac85cce4059
SadManFahIm <fahimsadman4@gmail.com>
2021-09-13 22:11:50 +0600

    dfd

--- search_history/settings.py
+++ search_history/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # custom user model
 AUTH_USER_MODEL = "accounts.CustomUser"
 
